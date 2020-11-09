05978ea890346645007ba80692b6ddff06a06bf6
Ubuntu <ubuntu@ip-172-31-10-2.ap-northeast-2.compute.internal>
2020-11-09 05:46:22 +0000

    server mysql ok

--- server_dev/server_dev/settings.py
+++ server_dev/server_dev/settings.py
@@ -130,8 +130,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ],
-}
+#REST_FRAMEWORK = {
+#    'DEFAULT_AUTHENTICATION_CLASSES': [
+#        'rest_framework.authentication.TokenAuthentication',
+#    ],
+#}
